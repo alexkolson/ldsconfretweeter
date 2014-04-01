1a6dddc1d8fbe687223d39fb3c3a854a9f9ad007
Alex Olson <alexkellenolson@gmail.com>
2014-03-31 21:22:04 -0600

    client config.

--- ldsgenconf.rb
+++ ldsgenconf.rb
@@ -1,2 +1,10 @@
 require 'twitter'
-puts "done"
+
+# construct client with our config
+
+lds_conf_client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "ImVo19PtwqH6kWkzecfIkw";
+    config.consumer_secret     = "W6icdXtPS6ipWJukJgLUcjplBsjUqa37lrL6Xf52Y6U";
+    config.access_token        = "275914972-YZfFIB5lfYLfjtu3DCE85yIxqbLwyJW7AiKt2aEK";
+    config.access_token_secret = "LtYFECJe6LzYU4AoE4UlLyVV5O7HazR9tdDAR67M";
+end
