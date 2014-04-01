66e0c067202471dae3d7a2be840180e06ec21f3c
Alex Olson <alexkellenolson@gmail.com>
2014-03-31 21:24:01 -0600

    no semicolons in ruby.

--- ldsgenconf.rb
+++ ldsgenconf.rb
@@ -2,8 +2,8 @@ require 'twitter'
 
 # construct client with our config
 ldsgenconf = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "ImVo19PtwqH6kWkzecfIkw";
-    config.consumer_secret     = "W6icdXtPS6ipWJukJgLUcjplBsjUqa37lrL6Xf52Y6U";
-    config.access_token        = "275914972-YZfFIB5lfYLfjtu3DCE85yIxqbLwyJW7AiKt2aEK";
-    config.access_token_secret = "LtYFECJe6LzYU4AoE4UlLyVV5O7HazR9tdDAR67M";
+    config.consumer_key        = "ImVo19PtwqH6kWkzecfIkw"
+    config.consumer_secret     = "W6icdXtPS6ipWJukJgLUcjplBsjUqa37lrL6Xf52Y6U"
+    config.access_token        = "275914972-YZfFIB5lfYLfjtu3DCE85yIxqbLwyJW7AiKt2aEK"
+    config.access_token_secret = "LtYFECJe6LzYU4AoE4UlLyVV5O7HazR9tdDAR67M"
 end
