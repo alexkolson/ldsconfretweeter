13f1fc2f482dce07944f69d00f6b8b28e2f3cf64
Alex Olson <alexkellenolson@gmail.com>
2014-04-01 17:02:51 -0600

    more additons to do not retewet list.

--- ldsgenconf.rb
+++ ldsgenconf.rb
@@ -8,14 +8,24 @@ ldsgenconf = Twitter::REST::Client.new do |config|
     config.access_token_secret = "LtYFECJe6LzYU4AoE4UlLyVV5O7HazR9tdDAR67M"
 end
 
-do_not_rt = [134902940, 79543857, 275914972, 2344076401, 973238479, 166001916, 1489117579]
+do_not_rt = [
+    134902940, 
+    79543857, 
+    275914972, 
+    2344076401, 
+    973238479, 
+    166001916, 
+    1489117579,
+    1120957878
+]
+
 count = 0
 
 ldsgenconf.search("#ldsconf", :since => Time.now.strftime("%Y-%m-%d")).select { |tweet| not do_not_rt.include? tweet[:user][:id] }.each do |tweet|
 
     # if this is a retweet, make sure the original tweet is
     # not from an undesirable user.
-    if tweet[:retweeted_status] && do_not_rt.include? tweet[:retweeted_status][:user][:id] 
+    if tweet[:retweeted_status] && do_not_rt.include?(tweet[:retweeted_status][:user][:id])
         puts "Original tweet is from an undesirable user. Not retweeting."
         next
     end
